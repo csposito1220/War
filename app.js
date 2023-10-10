7e927b0c9b9d55caf671a19419b9ab10f6bbc20b
csposito1220 <csposito1220@gmail.com>
2023-10-10 15:40:34 -0400

    Shuffle Deck and Card Value

--- app.js
+++ app.js
@@ -22,9 +22,11 @@ const values = [
   "K",
 ];
 
-function createDeck() {
-  const deck = [];
+const deck = [];
 
+// let running = false;
+
+function createDeck() {
   for (let i = 0; i < suits.length; i++) {
     for (let j = 0; j < values.length; j++) {
       deck.push(values[j] + "-" + suits[i]);
@@ -53,11 +55,70 @@ function getValue(card) {
   return parseInt(value);
 }
 
-function shuffleDeck() {
-  for (let i = 0; i < deck.length; i++) {
-    let j = Math.floor(Math.random() * deck.length);
-    let temp = deck[i];
-    deck[i] = deck[j];
-    deck[j] = temp;
+function shuffleDeck(deck) {
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+  console.log(deck);
+}
+
+function startGame() {
+  shuffleDeck(deck);
+  const player1Deck = deck.slice(0, 26);
+  const player2Deck = deck.slice(26);
+  let rounds = 0;
+
+  while (rounds < 5 && player1Deck.length > 0 && player2Deck.length > 0) {
+    const card1 = player1Deck.pop();
+    const card2 = player2Deck.pop();
+
+    if (values.indexOf(card1.value) > values.indexOf(card2.value)) {
+      player1Deck.unshift(card1, card2);
+    } else if (values.indexOf(card1.value) < values.indexOf(card2.value)) {
+      player2Deck.unshift(card1, card2);
+    } else {
+      const pot = [card1, card2];
+      let warWinner = null;
+
+      while (!warWinner) {
+        const card1War = player1Deck.pop();
+        const card2War = player2Deck.pop();
+        pot.unshift(card1War, card2War);
+
+        if (card1War && card2War) {
+          if (values.indexOf(card1War.value) > values.indexOf(card2War.value)) {
+            warWinner = "player1";
+          } else if (
+            values.indexOf(card1War.value) < values.indexOf(card2War.value)
+          ) {
+            warWinner = "player2";
+          }
+        } else {
+          // If one of the players runs out of cards during the war
+          warWinner = card1War ? "player1" : "player2";
+          break;
+        }
+      }
+
+      if (warWinner === "player1") {
+        player1Deck.unshift(...pot);
+      } else {
+        player2Deck.unshift(...pot);
+      }
+    }
+
+    rounds++;
+  }
+  displayWinner(player1Deck.length, player2Deck.length);
+}
+
+function displayWinner(player1Cards, player2Cards) {
+  if (player1Cards > player2Cards) {
+    document.querySelector("status-text").innerText = "Player1 Wins!";
+  } else if (player1Cards < player2Cards) {
+    document.querySelector("status-text").innerText = "Player2 Wins!";
+  } else {
+    document.querySelector("statues-text").innerText = "Draw";
   }
 }
